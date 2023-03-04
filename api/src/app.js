a6b6d458c71fd673ec25f1758684b0185e7a43a0
Victor Pinto <victorpintowake@gmail.com>
2023-03-04 13:50:00 -0300

    ruta del front protegida

--- api/src/app.js
+++ api/src/app.js
@@ -3,10 +3,12 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+require('dotenv').config();
 
 require('./db.js');
 
 const server = express();
+const { ACCESS_FRONTEND } = process.env;
 
 server.name = 'API';
 
@@ -15,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(`Access-Control-Allow-Origin', ${ACCESS_FRONTEND}`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');

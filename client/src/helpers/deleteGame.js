28aadd8dea6b306f88e804e33eb1907508b84ba0
Victor Pinto <victorpintowake@gmail.com>
2023-03-04 13:43:14 -0300

    helpers url

--- client/src/helpers/deleteGame.js
+++ client/src/helpers/deleteGame.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const deleteGame = async id => {
     try {
-        const res = await axios.delete('http://localhost:3001/game/' + id + '/delete');
+        const res = await axios.delete('https://henry-games.onrender.com/game/' + id + '/delete');
         if (res.status === 201)
             console.log('Videogame deleted successfully');
     } catch (err) {
